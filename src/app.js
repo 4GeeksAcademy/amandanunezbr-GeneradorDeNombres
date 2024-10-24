2af4135a3c94c8429e8092fa797a633b64fa6668
amandanunezbr <gerencialmonsters@gmail.com>
2024-10-24 17:44:14 +0000

    Projeto terminado

--- src/app.js
+++ src/app.js
@@ -9,3 +9,26 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+//
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+function generateDomains(pronouns, adjectives, nouns) {
+  let domains = [];
+
+  for (let p of pronouns) {
+    for (let a of adjectives) {
+      for (let n of nouns) {
+        let domain = `${p}${a}${n}.com`;
+        domains.push(domain);
+      }
+    }
+  }
+
+  return domains;
+}
+
+let allDomains = generateDomains(pronoun, adj, noun);
+
+console.log(allDomains);
