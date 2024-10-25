5863faf969e3bba0fdf0296e63479309d93ca61a
amandanunezbr <gerencialmonsters@gmail.com>
2024-10-25 18:23:20 +0000

    Cambios en el proyecto

--- src/app.js
+++ src/app.js
@@ -1,15 +1,3 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
-//
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
@@ -17,11 +5,10 @@ let noun = ["jogger", "racoon"];
 function generateDomains(pronouns, adjectives, nouns) {
   let domains = [];
 
-  for (let p of pronouns) {
-    for (let a of adjectives) {
-      for (let n of nouns) {
-        let domain = `${p}${a}${n}.com`;
-        domains.push(domain);
+  for (let pronoun of pronouns) {
+    for (let adj of adjectives) {
+      for (let noun of nouns) {
+        domains.push(`${pronoun}${adj}${noun}.com`);
       }
     }
   }
@@ -29,6 +16,4 @@ function generateDomains(pronouns, adjectives, nouns) {
   return domains;
 }
 
-let allDomains = generateDomains(pronoun, adj, noun);
-
-console.log(allDomains);
+console.log(generateDomains(pronoun, adj, noun));
